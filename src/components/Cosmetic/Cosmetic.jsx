9077f92a25c4803351264ad8e0c89254538c9136
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-04-08 06:12:27 +0600

    Create Event handler with parameters and without parameters try arrow function

--- src/components/Cosmetic/Cosmetic.jsx
+++ src/components/Cosmetic/Cosmetic.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+    
+    const addToCard = (id) => {
+        console.log('item added', id);
+    }
+
+    const addToCardWithParam = () => addToCard(id);
+
+
     return (
-        <div>
+        <div className='product'>
             <h2>buy this: {name}</h2>
             <h2>only for: ${price}</h2>
             <p><small>it has id: {id}</small></p>
+            <button onClick={addToCardWithParam}>Add to Card</button>
+            <button onClick={() => addToCard(id) }>Purchase</button> {/* : shortcut way */}
         </div>
     );
 };
